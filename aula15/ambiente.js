96b6c0cdd798d36dde5f00759fbc541e08edcdd7
Matheus Patriarca <matheussantanapatriarca2019@outlook.com>
2020-04-16 14:33:39 -0300

    Add files via upload

---
+++ aula15/ambiente.js
@@ -0,0 +1,9 @@
+let num = [4,8,2,9,3]
+
+num.push(1)
+
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+
+let pos = num.indexOf(8)
+console.log(pos)
---
+++ aula15/vetornatela.js
@@ -0,0 +1,7 @@
+let valores = [8,1,2,4,5,9]
+
+valores.sort()
+
+for(let i = 0;i < valores.length; i++) {
+    console.log(valores[i])
+}
